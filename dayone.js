4e93dd1cbcaeef9ecc498efcdadd0a6dbdaa9ba7
unknown <abhi.ghosh27feb@gmail.com>
2023-08-20 17:37:59 +0530

    first commit

---
+++ dayone.js
@@ -0,0 +1,31 @@
+// // const data=10;
+// // data=12;
+// let x = 1234567890123456789012345n;
+// console.log("x",x)
+//  console.log("x",typeof(x))
+//  let y = BigInt(1234567890123456789012345)
+//  console.log("y",y)
+//  console.log("y",typeof(y))
+// console.log("hey guys welcome to the js world")
+// var a=12
+// console.log("a",typeof(a))
+// a="hello"
+// console.log("a",typeof(a))
+ //num=false;
+ //console.log("type  of num",typeof(num))
+ // undifined and null
+//  var data;
+//  console.log("data",typeof(data))
+//  var mydata=null;
+//  console.log("mydata",typeof(mydata))
+
+// + - / *  ** arthemetic operator
+var no1=Number(prompt("enter first no"))
+var no2=Number(prompt("enter second no"))
+
+console.log("sum",no1+no2)
+console.log("sum",no1-no2)
+
+
+console.log("sum",no1*no2)
+console.log("sum",no1**no2)
